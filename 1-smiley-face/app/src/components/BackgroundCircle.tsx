2fd71f2a223a1f30a5e62976a6b630232352cdf1
itsmarkodowd <16547497+itsmarkodowd@users.noreply.github.com>
2021-08-19 22:53:51 +0100

    update: random background colour

--- 1-smiley-face/app/src/components/BackgroundCircle.tsx
+++ 1-smiley-face/app/src/components/BackgroundCircle.tsx
@@ -1,5 +1,19 @@
 import React from 'react'
 
+const getRandomColor = () => {
+  var letters = '0123456789ABCDEF'
+  var color = '#'
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)]
+  }
+  return color
+}
+
 export const BackgroundCircle = ({ radius, strokeWidth }) => (
-  <circle r={radius} fill="yellow" stroke="black" stroke-width={strokeWidth} />
+  <circle
+    r={radius}
+    fill={getRandomColor()}
+    stroke="black"
+    stroke-width={strokeWidth}
+  />
 )
